9fad91441519f1f3e41d4b2f6675642475842eb7
Maryam Shaikh <shaikhmaryam356@gmail.com>
2023-09-22 12:08:11 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  // register(){
+  //     this.router.navigate("register")
+  // }
+}
